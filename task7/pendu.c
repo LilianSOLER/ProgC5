c274691dbf4c186e818c99a27ee90345e9f6fa96
Lilian Soler <soler.lilian64@gmail.com>
2021-09-18 21:49:23 +0200

    Brouillon du main et de l'architecture du jeu du pendu v0.1

--- task7/pendu.c
+++ task7/pendu.c
@@ -9,11 +9,35 @@
 #include <time.h>
 
 #include "../task4/string.c"
+#include "function_pendu.c"
 
 int main(int nargs, char** args) {
 
   // initlalize the random generator
   srand(clock());
-  
+  do{
+    int nombre_aleatoire = rand() % 49;
+    int nombre_essai = nombre_essai_mot(nombre_aleatoire);
+    char* mot_a_deviner = mot_a_deviner(nombre_aleatoire);
+    char* mot_a_deviner_pour_afficher = creer_tiret(strlen(*mot_a_deviner);
+
+    while (nombre_essai != 0) {
+      printf("Vous avez %d coups pour deviner le mot :\n", nombre_essai);
+      printf("%s\n", mot_a_deviner_pour_afficher);
+      do{
+        printf("?");
+        char lettre = lire_lettre();
+        printf("\n");
+      }while(lettre == NULL);
+      if(verif_lettre(lettre, *mot_a_deviner, *mot_a_deviner_pour_afficher)){
+        printf("Bravo !\n");
+        break;
+      }     
+    }
+    if(nombre_essai == 0){
+      printf("Vous avez perdu !\n");
+      printf("Le mot était : %s\n", *mot_a_deviner);
+    }
+  }while(continuer_verif());
   printf("Bye.\n");
 }
